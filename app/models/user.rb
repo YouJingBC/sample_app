2e745d3de35617170389fe89c919f941581d168d
NoNonsense126 <retail_nns@hotmail.com>
2015-01-21 12:18:10 +0800

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ActiveRecord::Base
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 	has_many :microposts, dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",
+																	 foreign_key: "followed_id",
+																	 dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: true, length: { maximum:50 }
 	validates :email, presence: true, length: { maximum:255 },
-						format: { with: VALID_EMAIL_REGEX}, 
+						format: { with: VALID_EMAIL_REGEX }, 
 						uniqueness: { case_sensitive:false }
 	validates :password, length: { minimum: 6 }, allow_blank: true
 	has_secure_password
@@ -57,8 +65,23 @@ class User < ActiveRecord::Base
 		reset_sent_at < 2.hours.ago
 	end
 
-	def feed
-		Micropost.where("user_id = ?", id)
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	def following?(other_user)
+		following.include?(other_user)
 	end
 
 	private
